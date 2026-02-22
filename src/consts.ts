f4aa6e2f3c143f03a2ee1e350f340563a6bf0404
CyJackX <42789314+CyJackX@users.noreply.github.com>
2026-02-22 17:26:02 -0500

    Starting everything

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Andy Zou - Video Director & Creative Technologist";
+export const SITE_DESCRIPTION = "Andy Zou is a Director and Creative generalist who has worked on content of all shapes and sizes.  From socials at Comedy Central to branded promos for Broadway, Andy has a knack for finding a comedic tone that resonates with everyone.";
